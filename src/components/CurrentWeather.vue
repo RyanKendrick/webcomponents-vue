325e33585a29f46f8da78b67f0c675fd48199cc9
RyanKendrick <rpkendrick@gmail.com>
2020-07-07 18:14:22 -0400

    added openweathermap API key and CurrentWeather.vue component

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <br />
+        <b-list-group v-if="weather.main">
+            <b-list-group-item>Current Temperature:</b-list-group-item>
+            <b-list-group-item>High:</b-list-group-item>
+            <b-list-group-item>Low:</b-list-group-item>
+            <b-list-group-item>Pressure:</b-list-group-item>
+            <b-list-group-item>Humidity:</b-list-group-item>
+        </b-list-group>
+    </div>
+</template>
+
+<script>
+import { requestsMixin } from "@/mixins/requestsMixin";  
+import store from "../store";  
+import { BListGroup, BListGroupItem } from "bootstrap-vue";  
+import 'bootstrap/dist/css/bootstrap.css'  
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+    store,
+    name: "CurrentWeather",
+    mounted() {},
+    mixins: [requestMixin],
+    components: {
+        BListGroup,
+        BListGroupItem
+    },
+    computed: {
+        keyword() {
+            return this.$store.state.keyword
+        }
+    },
+    data() {
+        return {
+            weather: {}
+        };
+    },
+    watch: {
+        async keyword(val) {
+            const = response = await this.searchWeather(val);
+            this.weather = response.data;
+        }
+    }
+};
+</script>
+
+<style scoped>  
+p {  
+  font-size: 20px;  
+}  
+</style>
